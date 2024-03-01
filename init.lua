43e89fd7317ba88565d4d3fba00766352d548978
ItsLolgical <signupthing@itslolgical.com>
2024-03-01 07:19:48 -0600

    Updated references to fix pathing issues.

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
 if vim.g.vscode then
   -- VSCode extension
   -- Do nothing for now.
-  require("options.lua")
-  require("keybinds.lua")
+  require("options")
+  require("keybinds")
 else
 -- pick your plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -18,25 +18,25 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lua.options")
-require("lua.keybinds")
+require("options")
+require("keybinds")
 
 require("lazy").setup({
   { "Olical/nfnl", ft = "fennel" },
-  require("lua.alpha-config"),
-  require("lua.treesitter-config"),
-  require("lua.catppuccin-config"),
-  require("lua.which-key-config"),
-  require("lua.oil-config"),
-  require("lua.toggleterm-config"),
-  require("lua.lsp-plugins"),
-  require("lua.telescope-config"),
-  require("lua.fzf-config"),
-  require("lua.nvim-cmp-config"),
-  require("lua.conjure-config")
+  require("alpha-config"),
+  require("treesitter-config"),
+  require("catppuccin-config"),
+  require("which-key-config"),
+  require("oil-config"),
+  require("toggleterm-config"),
+  require("lsp-plugins"),
+  require("telescope-config"),
+  require("fzf-config"),
+  require("nvim-cmp-config"),
+  require("conjure-config")
 }, {})
 
-require("lua.lsp-config")
+require("lsp-config")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
