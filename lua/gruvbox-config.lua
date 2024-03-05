e6b45bc3f2766f59f7cc3c5638280667586a75bc
ItsLolgical <signupthing@itslolgical.com>
2024-03-04 21:59:56 -0600

    Added in gruvbox config.

---
+++ lua/gruvbox-config.lua
@@ -0,0 +1,7 @@
+-- [nfnl] Compiled from gruvbox-config.fnl by https://github.com/Olical/nfnl, do not edit.
+local gruvbox_config
+local function _1_()
+  return vim.cmd.colorscheme("gruvbox")
+end
+gruvbox_config = {"ellisonleao/gruvbox.nvim", name = "gruvbox", priority = 1000, config = _1_}
+return gruvbox_config
